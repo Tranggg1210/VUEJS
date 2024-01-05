fee3e2aef98ceb3c8265067f8c69ee8c429148a7
TrangNguyen1203 <nguyenthitrang.tte@gmail.com>
2024-01-05 20:55:41 +0700

    :seedling: create base vue and learning ref, reactive

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue';
+import Header from '@/components/Header.vue'
+
+let a = ref(1);
+const c = "hello";
+const disable = ref(false);
+const handleClick = () => {
+  a.value++;
+  if (a == 10) {
+    disable.value = true;
+    a.value = 0;
+  };
+}
+</script>
+
+<template>
+  <h1 :class="c" :style="{
+    padding: 20
+  }">{{ a }}</h1>
+  <button @click="handleClick" :disabled="disable">Click me</button>
+  <Header />
+</template>
+
+<style scoped>
+.hello {
+  background-color: green;
+  color: #fff;
+  display: block;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+        <nav>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </nav>
+    </header>
+</template>
+
+<script>
+
+</script>
+
+<style lang="css" scoped>
+li {
+    color: red;
+}
+</style>
